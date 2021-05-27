e5e72670a556acff32c0c992f009184e0b0cd17a
Non Alyya Yan Hari <59119597+alyya04@users.noreply.github.com>
2021-05-27 14:39:52 +0700

    Update app.py

--- Cloud/app.py
+++ Cloud/app.py
@@ -1,1 +1,41 @@
+from flask import Flask, render_template, request
+from keras.models import load_model
+from keras.preprocessing import image
 
+app = Flask(__name__)
+
+dic = {0.5: 'adalah gigi sehat.', 0.0 : 'adalah gigi tidak sehat.'}
+
+model = load_model('ISINAMAMODEL')
+
+model.make_predict_function()
+
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(224,224))
+	i = image.img_to_array(i)/255.0
+	# i = i.reshape(1, 100,100,3)
+  i=np.expand_dims(i, axis=0)
+	p = model.predict_classes(i)
+	return dic[p[0]]
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
