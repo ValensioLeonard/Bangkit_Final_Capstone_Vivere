f4a5b08f0b98c347edf3bcd43b6ee266281d4f14
Arif Hayati Indah Lestari <indahlestari1004@gmail.com>
2021-05-30 10:36:28 +0700

    update app.py API

--- Cloud/app.py
+++ Cloud/app.py
@@ -1,41 +1,64 @@
-from flask import Flask, render_template, request
-from keras.models import load_model
-from keras.preprocessing import image
+import numpy as np
+import os
+import tensorflow as tf
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from shutil import copyfile
+import re
+from shutil import move
+from werkzeug.utils import secure_filename
+import urllib.request
 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+
+# from keras.models import load_model
+# from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template, jsonify, flash
+#from flask_ngrok import run_with_ngrok
+
+# Define a flask app
 app = Flask(__name__)
+#run_with_ngrok(app)
 
-dic = {0.5: 'adalah gigi sehat.', 0.0 : 'adalah gigi tidak sehat.'}
+# Load your trained model
+model = tf.keras.models.load_model('modelv1.h5',custom_objects={'KerasLayer':hub.KerasLayer})
+model.make_predict_function()          # Necessary
 
-model = load_model('ISINAMAMODEL')
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
 
-model.make_predict_function()
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])/ 255.0
 
-def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(224,224))
-	i = image.img_to_array(i)/255.0
-	# i = i.reshape(1, 100,100,3)
-  i=np.expand_dims(i, axis=0)
-	p = model.predict_classes(i)
-	return dic[p[0]]
+    preds = model.predict(images)
+    if preds[0] > 0.5:
+      return "Gigi sehat."
+    else:
+      return "Gigi Karies."
 
-# routes
-@app.route("/", methods=['GET', 'POST'])
-def main():
-	return render_template("index.html")
+@app.route('/')
+def index():
+  return """
+          Application is working
+  """
 
-@app.route("/submit", methods = ['GET', 'POST'])
-def get_output():
-	if request.method == 'POST':
-		img = request.files['my_image']
+@app.route('/predict', methods=['POST', 'GET'])
+def upload_image():
+  if request.method == 'POST':
+    f = request.files['file']
+    file_path = secure_filename(f.filename)
+    f.save(file_path)
 
-		img_path = "static/" + img.filename	
-		img.save(img_path)
+    #make prediction
+    preds = model_predict(file_path, model)
+    return preds
 
-		p = predict_label(img_path)
-
-	return render_template("index.html", prediction = p, img_path = img_path)
-
-
-if __name__ =='__main__':
-	#app.debug = True
-	app.run(debug = True)
+if __name__ == '__main__':
+    # app.debug = True
+    app.run()
