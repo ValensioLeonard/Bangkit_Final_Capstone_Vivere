2765e89c8e394b2f5a130dcef526a27fbbd566b3
Non Alyya Yan Hari <59119597+alyya04@users.noreply.github.com>
2021-06-01 16:58:08 +0700

    Update and rename app.py to main.py

--- Cloud/main.py
+++ Cloud/main.py
@@ -1,38 +1,31 @@
+import cv2
+import io
 import numpy as np
-import os
+from numpy import asfarray
 import tensorflow as tf
 import tensorflow_hub as hub
-import matplotlib.pyplot as plt
 from tqdm import tqdm
-from shutil import copyfile
-import re
-from shutil import move
 from werkzeug.utils import secure_filename
 import urllib.request
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 
-# from keras.models import load_model
-# from keras.preprocessing import image
-
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template, jsonify, flash
-#from flask_ngrok import run_with_ngrok
 
 # Define a flask app
 app = Flask(__name__)
-#run_with_ngrok(app)
 
 # Load your trained model
 model = tf.keras.models.load_model('modelv1.h5',custom_objects={'KerasLayer':hub.KerasLayer})
 model.make_predict_function()          # Necessary
 
 def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = img_path
 
-    # Preprocessing the image
-    x = image.img_to_array(img)
+    x = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+    x = cv2.resize(x,(224,224))
     x = np.expand_dims(x, axis=0)
     images = np.vstack([x])/ 255.0
 
@@ -52,13 +45,14 @@ def index():
 def upload_image():
   if request.method == 'POST':
     f = request.files['file']
-    file_path = secure_filename(f.filename)
-    f.save(file_path)
+    # file_path = secure_filename(f.filename)
+    # f.save(file_path)
 
     #make prediction
-    preds = model_predict(file_path, model)
+    # preds = model_predict(file_path, model)
+    preds = model_predict(f, model)
     return preds
 
 if __name__ == '__main__':
     # app.debug = True
-    app.run()
+    app.run(debug=True)
